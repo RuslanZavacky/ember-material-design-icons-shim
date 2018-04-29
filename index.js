dfc7ba8cee666f06804d732259941c1b3ce7529e
Ruslan Zavacky <ruslan.zavacky@gmail.com>
2018-04-29 18:55:11 +0100

    Use this.import to add addon to the root app

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ module.exports = {
   name: 'ember-material-design-icons-shim',
   included(appOrAddon) {
     this._super.included(appOrAddon);
-    let app = appOrAddon;
-    if (typeof appOrAddon.import !== 'function' && appOrAddon.app) {
-      app = appOrAddon.app;
-    }
-    this.app = app;
-    app.import('vendor/iconfont/MaterialIcons-Regular.eot', { destDir: 'assets' });
-    app.import('vendor/iconfont/MaterialIcons-Regular.ijmap', { destDir: 'assets' });
-    app.import('vendor/iconfont/MaterialIcons-Regular.svg', { destDir: 'assets' });
-    app.import('vendor/iconfont/MaterialIcons-Regular.ttf', { destDir: 'assets' });
-    app.import('vendor/iconfont/MaterialIcons-Regular.woff', { destDir: 'assets' });
-    app.import('vendor/iconfont/MaterialIcons-Regular.woff2', { destDir: 'assets' });
-    app.import('vendor/iconfont/material-icons.css');
+
+    this.import('vendor/iconfont/MaterialIcons-Regular.eot', { destDir: 'assets' });
+    this.import('vendor/iconfont/MaterialIcons-Regular.ijmap', { destDir: 'assets' });
+    this.import('vendor/iconfont/MaterialIcons-Regular.svg', { destDir: 'assets' });
+    this.import('vendor/iconfont/MaterialIcons-Regular.ttf', { destDir: 'assets' });
+    this.import('vendor/iconfont/MaterialIcons-Regular.woff', { destDir: 'assets' });
+    this.import('vendor/iconfont/MaterialIcons-Regular.woff2', { destDir: 'assets' });
+    this.import('vendor/iconfont/material-icons.css');
   }
 };
